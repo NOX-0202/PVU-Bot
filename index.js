ab3baa09aa794f721fc1cbbfbde8a57c68365077
NOX-0202 <fernandinho.nco@gmail.com>
2021-08-25 20:19:38 -0300

    Files

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js")
+const opn = require('open')
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const Config = require("./config.json")
+
+client.on("ready", async () => {
+    console.log('Bot iniciado')
+})
+
+client.on("messageCreate", async message => {
+
+    if (message.channel.type === "DM") return
+
+    const args = message.content.slice(Config.prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+
+    if (
+        message.channel.id === Config.pvu_channel_ID || 
+        message.channel.id === Config.channel_ID
+    ) {
+        let link = message.content.trim().split('https')
+        let link_formated = 'https' + link[1]
+        console.log(link_formated)
+        if (link_formated.indexOf("https://") != -1) {
+            console.log('abrindo as tabs')
+            await opn(link_formated, {
+                app: {
+                    name: opn.apps.edge
+                }
+            })        
+        }
+
+
+    }
+
+    if (message.channel.id === '879826240457043999') {
+        let m = client.channels.cache.get(Config.channel_ID).send(message.content)
+        setTimeout(() => { m.delete() }, 120000)
+    }
+})
+
+client.login(Config.token)
