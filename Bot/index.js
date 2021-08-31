ab9b5c78069050674f11f5003b2c6b3b4eae5a16
NOX-0202 <fernandinho.nco@gmail.com>
2021-08-31 01:38:41 -0300

    gettting config data to bot and web

--- Bot/index.js
+++ Bot/index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-const Config = require("./config.json")
+const Config = require("./../config.json")
 
 client.on("ready", async () => {
     console.log('Bot iniciado')
