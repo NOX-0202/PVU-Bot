1351557d2ce3641700e6823e2678a991a61e56db
NOX-0202 <fernandinho.nco@gmail.com>
2021-09-01 12:37:34 -0300

    code optimization

--- Bot/index.js
+++ Bot/index.js
@@ -1,25 +1,48 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const http = require('http')
 const Config = require("./../config.json")
 
 client.on("ready", async () => {
     console.log('Bot iniciado')
 })
 
-client.on("message", async message => {
+client.on("messageCreate", async message => {
 
     if (message.channel.type === "DM") return
 
     const args = message.content.slice(Config.prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase()
 
-    if (message.channel.id === Config.farmers_channel_ID) {
-        let m = client.channels.cache.get(Config.channel_ID).send(message.content)
+    if (message.channel.id === Config.pvu_channel_ID) {
+        let m = client.channels.cache.get(Config.pvu_channel_ID).send(message.content)
+        makereq(message.content)
         await setTimeout(() => {
             console.log('Deletando a mensagem')
              m.delete()
         }, 120000)
     }
 })
+
+const makereq = link => {
+
+    http.get(`http://localhost:3000/get-link/${link}`, resp => {
+        let data = '';
+      
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          console.log('foi');
+        });
+      
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+
+}
 
 client.login(Config.token)
--- web/index.js
+++ web/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const http = require('http');
+const http = require('http').Server(app)
 const { Server } = require("socket.io");
 
 var app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(http)
 
 app.get('/', (req, res) => {  
     res.sendFile(__dirname + '/index.html');    
@@ -12,21 +11,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/get-link/:link', (req, res) => {  
-    io.emit('link', { link: req.params.link }); // This will emit the event to all connected sockets
+    io.emit('link', () =>  { link: req.params.link }); // This will emit the event to all connected sockets
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('link', (msg) => {
+
+
+    socket.on('link', msg => {
         io.emit('link', msg);
-      });
+    });
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
-  });
+    
+});
 
 
 
-server.listen(3000, () => {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
