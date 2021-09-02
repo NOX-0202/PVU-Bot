d1c31cb5daad7a60bc3e69f4fb63ea0d86ca093a
NOX-0202 <fernandinho.nco@gmail.com>
2021-09-02 17:57:59 -0300

    sockets configuration issue

--- Bot/index.js
+++ Bot/index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-const http = require('http')
+const http = require('https')
 const Config = require("./config.json")
 var previousLink = '1'
 
@@ -33,7 +33,7 @@ client.on("messageCreate", async message => {
 
 function makeReq(link) {
 
-  http.get(`http://localhost:4567/getLink/${encodeURIComponent(link)}`, resp => {
+  http.get(`https://pvu-bot-backend.herokuapp.com/getLink/${encodeURIComponent(link)}`, resp => {
     let data = '';
   
     // A chunk of data has been received.
