a9692b089cd714c379ed51e77d76f16071bd7af3
NOX-0202 <fernandinho.nco@gmail.com>
2021-09-02 21:31:43 -0300

    bot config

--- Bot/index.js
+++ Bot/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const http = require('https')
-const Config = require("./config.json")
+const Config = require("./config_test.json")
 var previousLink = '1'
 
 client.on("ready", async () => {
